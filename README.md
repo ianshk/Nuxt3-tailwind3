38c391f3aae42d95436ff4afece56a61fa31cf49
ianshk <ianshk@gmail.com>
2022-01-06 14:50:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Nuxt 3 Tailwind 3
+ Nuxt 3, Tailwind 3 setup
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container mx-auto">
+    <div class="text-center text-lg">Nuxt, Tailwind 3</div>
+  </div>
+</template>
